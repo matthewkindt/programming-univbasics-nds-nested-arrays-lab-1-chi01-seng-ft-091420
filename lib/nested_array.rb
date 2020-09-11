87e490f3f962407913bf16acedf5982eec18ac3a
Matthew Kindt <matthewkindt@yahoo.com>
2020-09-11 19:23:51 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -34,7 +34,7 @@ end
 
 def matrix_lookup(matrix, row, column)
 matrix, row, column[0][1][1] 
-matrix, row, column[1][1][1]
+
 
   
   # Given any matrix (array of arrays), a row index and a column index, 

45c406aaf9b9ea32cc32ac6795a84692a8befde9
Matthew Kindt <matthewkindt@yahoo.com>
2020-09-11 20:28:00 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -33,7 +33,7 @@ def sorted_matrix
 end
 
 def matrix_lookup(matrix, row, column)
-([matrix[1]][row[1]][column[0]])
+[matrix[1]][row[1]][column[0]
 
   
   # Given any matrix (array of arrays), a row index and a column index, 
@@ -41,7 +41,7 @@ def matrix_lookup(matrix, row, column)
 end
 
 def matrix_update(matrix, row, column, new_value)
-  [matrix[0]][row[1]][column[2]]
+  [matrix[1]][row[0]][column[2]]
   [matrix[0]]
   # Given any matrix (array of arrays), a row index and a column index, 
   # Update the matrix location at that row and column to have the value of new_value
